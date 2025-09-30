992656e258b1a768064ef29565a44498d3f5a581
Lucas araújo <lucas.zimovski.brasil@escola.pr.gov.br>
2025-09-30 09:53:29 -0300

    ç

---
+++ jogos/src/teste/janelateste.java
@@ -0,0 +1,14 @@
+package teste;
+
+import javax.swing.JFrame;
+
+public class janelateste {
+    public static void main(String[] args) {
+        JFrame janela= new JFrame("janelas");
+janela.setBounds(0,0,1920,1080);
+janela.setVisible(true);
+
+
+    }
+
+}
