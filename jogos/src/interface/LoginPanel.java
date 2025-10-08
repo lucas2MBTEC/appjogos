b1e2433d4779290731770ff7fe867578310894d8
Lucas araújo <lucas.zimovski.brasil@escola.pr.gov.br>
2025-10-08 09:49:56 -0300

    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- jogos/src/interface/LoginPanel.java
+++ jogos/src/interface/LoginPanel.java
@@ -1,6 +1,7 @@
 package
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
  interface;
@@ -8,6 +9,21 @@ import javax.swing.JPanel;
 public class LoginPanel extends  TelaPanel {
 private JButton botaoLogin;
     public LoginPanel(JPanel telas, JFrame janela){
-        
+        JLabel mensagem = new JLabel("conteudo de Login Panel ");
+JLabel botao = new JButton("Fazer login");
+botao.addActionListener(this);
+this.add(mensagem);
+this.add(botao);
+
+
+
+
+    }
+
+    public void executarBotao(ActionEvent e){
+        trocaTela("Tela Principal");
+
+
+
     }
 }
