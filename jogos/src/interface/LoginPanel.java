7c6362e0518f1cc0c90c9f9cf53a32766afdf695
Lucas araújo <lucas.zimovski.brasil@escola.pr.gov.br>
2025-10-28 09:55:19 -0300

    dw

--- jogos/src/interface/LoginPanel.java
+++ jogos/src/interface/LoginPanel.java
@@ -1,29 +1,26 @@
-package
+package inteface LoginPanel;
+
+import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
- interface;
+public class LoginPanel extends TelaPanel{// extende a TelaPanel
+    private JButton botaoLogin;//método JButton
+    public LoginPanel(JPanel telas, JFrame janela) { //metodo contrutor
+        super(telas,janela);
+        JLabel mensagem = new JLabel("Conteúdo do Login Panel");//CRIA UMA MENSAGEM
 
-public class LoginPanel extends  TelaPanel {
-private JButton botaoLogin;
-    public LoginPanel(JPanel telas, JFrame janela){
-        JLabel mensagem = new JLabel("conteudo de Login Panel ");
-JLabel botao = new JButton("Fazer login");
-botao.addActionListener(this);
-this.add(mensagem);
-this.add(botao);
+        JButton botao = new JButton("Fazer login"); //CRIA UM BOTÃO
+        botao.addActionListener(this);//chama o método override
 
-
-
-
+        this.add(mensagem);//ADICIONA A MENSAGEM NA TELA
+        this.add(botao);// ADICIONA O BOTÃO NA TELA
+        //this.add(imagem);//adiciona imagem
     }
 
     public void executarBotao(ActionEvent e){
-        trocaTela("Tela Principal");
-
-
-
+        trocarTela("Tela Principal");
     }
 }
--- jogos/src/interface/PrincipalPanel.java
+++ jogos/src/interface/PrincipalPanel.java
@@ -1,29 +1,24 @@
-package
+package inteface PrincipalPanel;
+
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
- interface;
+public class PrincipalPanel extends TelaPanel{
 
-public class PrincipalPanel  extends TelaPanel{
-    public PrincipalPanel(JPanel telas, JFrame janela){
-super(telas,janela);
-JLabel mensagem = new JLabel("conteúdo de pricipal panel");
-JButton botao= new JButton("voltar");
-botao.addActionListener(this);
-this.add(botao);
-this.add(mensagem);
+    public PrincipalPanel(JPanel telas, JFrame janela){//jpanel telas para ser exibido na trca de telas
+        super(telas, janela);
+        JLabel mensagem = new JLabel(" Conteúdo do principal Panel");//CRIA A MENSAGEM
+        JButton botao = new JButton("voltar"); //CRIA UM BOTÃO
+        botao.addActionListener(this);//chama o método override
 
-
-
-
+        this.add(botao);
+        this.add(mensagem);//ADICIONA A MENSAGEM NA TELA
     }
-public void executarBotao(ActionEvent e ){
-    trocaTela("Tela Login");
-}
 
-
-
+    public void executarBotao(ActionEvent e){
+        trocarTela("Tela Login");
+    }
 }
